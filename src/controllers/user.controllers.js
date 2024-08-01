63f573e3f9e150429254f51005f02602fd3cfee9
AhmedArbaz <ah2797764@gmail.com>
2024-08-01 12:29:08 +0500

    adding validation for coverImage if we send empty so don't give error because our coverimage is not a required field

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -71,7 +71,13 @@ if(existedUser){
 
 
 const avatarLocalPath = req.files?.avatar[0]?.path;
-const coverImageLocalPath = req.files?.coverImage[0]?.path;
+// const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+// This checks if there is a cover image or not because we don't add it required so we add validation
+let coverImageLocalPath;
+if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+    coverImageLocalPath = req.files.coverImage[0].path
+}
 
 if(!avatarLocalPath){
     throw new ApiError(400,"Avatar file is required")
